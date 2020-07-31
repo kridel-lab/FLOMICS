77f2289b20a628226f3d109ce785c80b735086d3
sarah-russell2 <67651011+sarah-russell2@users.noreply.github.com>
2020-07-31 13:10:15 -0400

    files for MiXCR analysis: unique clonotype count

---
+++ Code/BioinformaticsProcessing/RNAseq/MiXCR/001_run_mixcr_merge.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -p himem
+#SBATCH --mem=60440M
+#SBATCH -t 5-00:00 # Runtime in D-HH:MM
+#SBATCH -J mixcr_matrix
+
+module load java/8  #8
+module load samtools
+module load python3
+module load gatk
+module load annovar
+module load bedtools
+module load R/3.6.1
+
+Rscript /cluster/home/srussell/mixcr_concatenate_all_clonotypes.R
