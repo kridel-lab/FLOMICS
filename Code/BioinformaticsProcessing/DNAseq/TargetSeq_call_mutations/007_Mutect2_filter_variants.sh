e25199efe538998bc77302aaebebf5323eb1d23c
karini925 <karin.isaev@gmail.com>
2020-08-11 13:40:19 -0400

    update script for variant filtering

--- Code/BioinformaticsProcessing/DNAseq/TargetSeq_call_mutations/007_Mutect2_filter_variants.sh
+++ Code/BioinformaticsProcessing/DNAseq/TargetSeq_call_mutations/007_Mutect2_filter_variants.sh
@@ -13,28 +13,35 @@ module load python3
 module load gatk
 module load annovar
 
-cd /cluster/projects/kridelgroup/FLOMICS/ANALYSIS/MUTECT2
-ls *.vcf.gz > all_vcf_files_FL
+cd /cluster/projects/kridelgroup/FLOMICS/DATA/TargetedDNAseq/BC_TargetSeq_Aug2020
+#ls *.bam > all_bam_files_FL
 
-samples=all_vcf_files_FL
+samples=all_bam_files_FL
 names=($(cat $samples))
 sample=${names[${SLURM_ARRAY_TASK_ID}]}
 echo $sample
 
 gtf_file=/cluster/projects/kridelgroup/FLOMICS/genome_files/gencode.v19.annotation.gtf
 fasta_file=/cluster/projects/kridelgroup/FLOMICS/genome_files/human_g1k_v37.decompressed.fasta
-out_folder=/cluster/projects/kridelgroup/FLOMICS/ANALYSIS/MUTECT2
+var_folder=/cluster/projects/kridelgroup/FLOMICS/ANALYSIS/MUTECT2
+out_folder=/cluster/projects/kridelgroup/FLOMICS/ANALYSIS/MUTECT2/pileup_summaries
 #targets_interval_list=/cluster/projects/kridelgroup/FLOMICS/DATA/TargetedDNAseq/picard_tools_targets_input.bed
 targets_interval_list=/cluster/projects/kridelgroup/FLOMICS/DATA/TargetedDNAseq/picard_tools_amps_input.bed
 ints=/cluster/projects/kridelgroup/FLOMICS/DATA/TargetedDNAseq/${sample}_targets.interval_list
 
-gatk Mutect2 \
--R $fasta_file \
--I ${names[${SLURM_ARRAY_TASK_ID}]} \
--tumor ${tum} \
--L $ints \
--O $out_folder/${tum}.vcf.gz \
---germline-resource /cluster/projects/kridelgroup/RAP_ANALYSIS/af-only-gnomad.raw.sites.b37.vcf.gz
+tum=($(samtools view -H ${names[${SLURM_ARRAY_TASK_ID}]} | grep '^@RG' | sed "s/.*SM:\([^\t]*\).*/\1/g" | uniq))
+echo "${tum}"
+export tum
+
+gatk GetPileupSummaries \
+  -I ${sample} \
+  -V /cluster/projects/kridelgroup/RAP_ANALYSIS/af-only-gnomad.raw.sites.b37.vcf.gz \
+  -L $ints \
+  -O $out_folder/${tum}_getpileupsummaries.table
+
+gatk CalculateContamination \
+  -I $out_folder/${tum}_getpileupsummaries.table \
+  -O $out_folder/${tum}_calculatecontamination.table
 
 gatk FilterMutectCalls -R $fasta_file \
--V $sample -O $sample_filtered.vcf.gz
+  -V $var_folder/${tum}.vcf.gz -O $var_folder/${tum}_filtered.vcf.gz
