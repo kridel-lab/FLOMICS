9fd52b3ce0aff2c0e4552c80b8dda5a1b717a061
sarah-russell2 <67651011+sarah-russell2@users.noreply.github.com>
2020-08-12 12:31:47 -0400

    Update telescope_erv_visualization.R

--- Code/BioinformaticsProcessing/RNAseq/ERVsDetection/telescope_erv_visualization.R
+++ Code/BioinformaticsProcessing/RNAseq/ERVsDetection/telescope_erv_visualization.R
@@ -40,15 +40,18 @@ setwd("/cluster/projects/kridelgroup/FLOMICS/ANALYSIS/TELESCOPE_ANALYSIS/edgeR_t
 #----------------------------------------------------------------------------------
 #DATA
 #----------------------------------------------------------------------------------
-all_de_stages=fread("/cluster/projects/kridelgroup/FLOMICS/ANALYSIS/TELESCOPE_ANALYSIS/edgeR_telescope/_2020-07-16_all_DE_ervs_stage.csv")
-all_de_clusters=fread("/cluster/projects/kridelgroup/FLOMICS/ANALYSIS/TELESCOPE_ANALYSIS/edgeR_telescope/_2020-07-16_all_DE_ervs_cluster.csv")
-ervs_de=rbind(all_de_stages, all_de_clusters)
+ervs_de=fread("/cluster/projects/kridelgroup/FLOMICS/ANALYSIS/TELESCOPE_ANALYSIS/edgeR_telescope/_2020-08-12_de_ervs_STAGE_CLUSTER.csv")
+dim(ervs_de)
+#[1] 29050      8
 
-ervs_de_sig = unique(filter(ervs_de, FDR < 0.05)$transcript)
-#get all significant ERVs (FDR < 0.05)
-ervs_de$diff_exp = ""
-ervs_de$diff_exp[ervs_de$logFC >0 ] ="Upregulated"
-ervs_de$diff_exp[ervs_de$logFC <0 ] ="Downregulated"
+ervs_de_sig = filter(ervs_de, FDR < 0.05)
+dim(ervs_de_sig)
+#[1] 5195    8
+
+#label neg/pos logFC (FDR < 0.05)
+ervs_de_sig$diff_exp = ""
+ervs_de_sig$diff_exp[ervs_de_sig$logFC >0 ] ="Upregulated"
+ervs_de_sig$diff_exp[ervs_de_sig$logFC <0 ] ="Downregulated"
 #----------------------------------------------------------------------------------
 #ANALYSIS
 #----------------------------------------------------------------------------------
@@ -57,13 +60,20 @@ ervs_de$diff_exp[ervs_de$logFC <0 ] ="Downregulated"
 # :::::::::::::::::::::::::::::::::::::::::::::::::::
 
 #Only DE ERVs
-ggdensity(filter(ervs_de, transcript_id %in% ervs_de_sig), x = "logFC",
+pdf("/cluster/home/srussell/DE_ERVs_totaldensities_FL.pdf", width=7, height=4)
+ggdensity(ervs_de_sig[ervs_de_sig$contrast=="ADVANCED_LIMITED",], x = "logFC",
           add = "mean", rug = TRUE, alpha=0.1,
-          color = "diff_exp", fill = "diff_exp") + ggtitle("Fold changes across QC tiers for DE ERVs (FDR < 0.05), n=X")+theme_bw()
+          color = "diff_exp", fill = "diff_exp") + ggtitle("Fold changes all QC tiers for DE ERVs in ADV-LIM (FDR < 0.05), n=798")+theme_bw()
+dev.off()
+
+pdf("/cluster/home/srussell/DE_ERVs_totaldensities_CLUS.pdf", width=7, height=4)
+ggdensity(ervs_de_sig[ervs_de_sig$contrast=="Cluster1_Cluster2",], x = "logFC",
+          add = "mean", rug = TRUE, alpha=0.1,
+          color = "diff_exp", fill = "diff_exp") + ggtitle("Fold changes all QC tiers for DE ERVs in C1-C2 (FDR < 0.05), n=4397")+theme_bw()
 dev.off()
 
 #make comparison matrix
-ervs_de_dat = as.data.table(filter(ervs_de, FDR < 0.05))
+ervs_de_dat = as.data.table(ervs_de_sig)
 barplot_sum = as.data.table(table(ervs_de_dat$contrast, ervs_de_dat$tier, ervs_de_dat$diff_exp))
 colnames(barplot_sum) = c("contrast", "tier", "Expression", "num_ervs")
 barplot_sum$Expression = factor(barplot_sum$Expression, levels=c("Upregulated", "Downregulated"))
@@ -71,22 +81,20 @@ barplot_sum$num_ervs[barplot_sum$Expression == "Downregulated"] = -(barplot_sum$
 barplot_sum$N = abs(barplot_sum$num_ervs)
 
 z = which(str_detect(barplot_sum$contrast, "ADVANCED_LIMITED"))
-barplot_sum$contrast[z] = "Adv vs Lim"
+barplot_sum$contrast[z] = "Adv_vs_Lim"
 z = which(str_detect(barplot_sum$contrast, "Cluster1_Cluster2"))
-barplot_sum$contrast[z] = "C1 vs C2"
+barplot_sum$contrast[z] = "C1_vs_C2"
 
 
-pdf("Down_Up_regulated_ERVs.pdf", width=5, height=4)
+pdf("/cluster/home/srussell/Down_Up_regulated_ERVs.pdf", width=5, height=4)
 g=ggbarplot(barplot_sum, x="contrast", y="num_ervs", label = barplot_sum$N , lab.size=2, fill="Expression", facet.by=c("tier"),
   palette=c("red", "blue"))+
 theme_bw()+
 theme(text = element_text(size=10)) + ylab("Number of ERVs") + xlab("Contrast")
-ggpar(g, legend="bottom") + geom_hline(yintercept=0)
+ggpar(g, legend="bottom", main="ERV Expression in ADV v. LIM/ C1 v. C2") + geom_hline(yintercept=0)
 dev.off()
 
-#how did you get the N label for downregulated below the x axis?
 # :::::::::::::::::::::::::::::::::::::::::::::::::::
-#having an issue getting the distribution of this graph right.. any suggestions?
 
 #for all sig ERVs, make histo plot of FCs
 ervs_de_histo = ervs_de_dat
@@ -104,8 +112,8 @@ plot_cons_cluster$contrast = factor(plot_cons_cluster$contrast, levels=c("Cluste
 plot_cons_stage$diff_exp = factor(plot_cons_stage$diff_exp, levels=c("Upregulated", "Downregulated"))
 plot_cons_cluster$diff_exp = factor(plot_cons_cluster$diff_exp, levels=c("Upregulated", "Downregulated"))
 
-plot_cons_stage$tier = factor(plot_cons_stage$tier, levels=c("Tier 1", "Tier 2", "Tier 3"))
-plot_cons_cluster$tier = factor(plot_cons_cluster$tier, levels=c("Tier 1", "Tier 2", "Tier 3"))
+plot_cons_stage$tier = factor(plot_cons_stage$tier, levels=c("tier_1", "tier_2", "tier_3"))
+plot_cons_cluster$tier = factor(plot_cons_cluster$tier, levels=c("tier_1", "tier_2", "tier_3"))
 #could probably put above in function
 
 
@@ -115,8 +123,8 @@ g=gghistogram(plot_cons_stage, x = "logFC",
    color = "diff_exp",
    fill = "diff_exp", palette = c("red", "blue"),
    add_density = TRUE, ggtheme=theme_bw(),
-   facet.by = "tier")+xlim(0,10)
-ggpar(g, legend="bottom")+theme(text = element_text(size=12))+ylab("Count")+xlab("logFC")
+   facet.by = "tier")
+ggpar(g, legend="bottom", legend.title = "Expression", main="Distribution of ERV logFC in ADV v. LIM")+theme(text = element_text(size=12))+ylab("Count")+xlab("Absolute logFC")
 dev.off()
 
 pdf("/cluster/home/srussell/cluster_histo.pdf", width=7, height=4)
@@ -125,7 +133,7 @@ g=gghistogram(plot_cons_cluster, x = "logFC",
    color = "diff_exp",
    fill = "diff_exp", palette = c("red", "blue"),
    add_density = TRUE, ggtheme=theme_bw(),
-   facet.by = "tier")+xlim(0,10)
-ggpar(g, legend="bottom")+theme(text = element_text(size=12))+ylab("Count")+xlab("logFC")
+   facet.by = "tier")
+ggpar(g, legend="bottom", legend.title = "Expression", main="Distribution of ERV logFC in C1 v. C2" )+theme(text = element_text(size=12))+ylab("Count")+xlab("Absolute logFC")
 dev.off()
 # :::::::::::::::::::::::::::::::::::::::::::::::::::
