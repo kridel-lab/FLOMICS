612f1c50dfd007236414a5209d67443ccb810cd2
karini925 <karin.isaev@gmail.com>
2020-08-31 10:54:04 -0400

    job for kallisto to make matrix

---
+++ Code/BioinformaticsProcessing/RNAseq/Kallisto/tximport_001_import_kallisto_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -p himem
+#SBATCH --mem=30440M
+#SBATCH -t 2-00:00 # Runtime in D-HH:MM
+
+cd /cluster/projects/kridelgroup/FLOMICS
+
+Rscript /cluster/home/kisaev/FLOMICS/Code/BioinformaticsProcessing/RNAseq/Kallisto/tximport_001_import_kallisto_data.R
