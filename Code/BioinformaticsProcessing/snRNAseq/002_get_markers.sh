154f984ed861d2025cc686adba7cb60aeb064138
sarah-russell2 <67651011+sarah-russell2@users.noreply.github.com>
2020-12-17 18:21:51 -0500

    Create 002_get_markers.sh

---
+++ Code/BioinformaticsProcessing/snRNAseq/002_get_markers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH -N 1 # Ensure that all cores are on one machine
+#SBATCH -p himem
+#SBATCH -c 6
+#SBATCH --mem=40440M
+#SBATCH -t 5-00:00 # Runtime in D-HH:MM
+#SBATCH -J seurat_run
+
+module load R/4.0.0
+
+#pwd
+cd /cluster/projects/kridelgroup/FLOMICS
+
+Rscript /cluster/home/kisaev/FLOMICS/Code/BioinformaticsProcessing/snRNAseq/002A_Seurat.R
+Rscript /cluster/home/kisaev/FLOMICS/Code/BioinformaticsProcessing/snRNAseq/002B_Seurat.R
