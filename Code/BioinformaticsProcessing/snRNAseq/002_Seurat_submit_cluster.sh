e17d96ec2b5d8a99645d061888a3ff6799664699
karini925 <karin.isaev@gmail.com>
2021-02-23 20:43:09 -0500

    update cluster script

--- Code/BioinformaticsProcessing/snRNAseq/002_Seurat_submit_cluster.sh
+++ Code/BioinformaticsProcessing/snRNAseq/002_Seurat_submit_cluster.sh
@@ -21,9 +21,9 @@ cd /cluster/projects/kridelgroup/FLOMICS
 #Rscript /cluster/home/kisaev/FLOMICS/Code/BioinformaticsProcessing/snRNAseq/002_Seurat_sarah_final_code_visualize_genes_across_clusters.R $file_name
 
 #standard workflow PC only
-file_name=/cluster/projects/kridelgroup/FLOMICS/ANALYSIS/snRNAseq/seurat/Feb2020/pc_genes_only_yes_seurat_integrated_dim_20_2000_2021-02-19_samples_clusters.rds
-Rscript /cluster/home/kisaev/FLOMICS/Code/BioinformaticsProcessing/snRNAseq/002_Seurat_sarah_final_code_visualize_genes_across_clusters.R $file_name
+#file_name=/cluster/projects/kridelgroup/FLOMICS/ANALYSIS/snRNAseq/seurat/Feb2020/pc_genes_only_yes_seurat_integrated_dim_20_2000_2021-02-19_samples_clusters.rds
+#Rscript /cluster/home/kisaev/FLOMICS/Code/BioinformaticsProcessing/snRNAseq/002_Seurat_sarah_final_code_visualize_genes_across_clusters.R $file_name
 
 #standard workflow all genes
-#file_name=/cluster/projects/kridelgroup/FLOMICS/ANALYSIS/snRNAseq/seurat/pc_genes_only_no_seurat_integrated_dim_20_2000_2021-02-17_samples_clusters.rds
-#Rscript /cluster/home/kisaev/FLOMICS/Code/BioinformaticsProcessing/snRNAseq/002_Seurat_sarah_final_code_visualize_genes_across_clusters.R $file_name
+file_name=/cluster/projects/kridelgroup/FLOMICS/ANALYSIS/snRNAseq/seurat/Feb2020/pc_genes_only_no_seurat_integrated_dim_20_2000_2021-02-23_samples_clusters.rds
+Rscript /cluster/home/kisaev/FLOMICS/Code/BioinformaticsProcessing/snRNAseq/002_Seurat_sarah_final_code_visualize_genes_across_clusters.R $file_name
