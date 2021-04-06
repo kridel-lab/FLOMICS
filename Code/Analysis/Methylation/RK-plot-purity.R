d67a2f2aeec1bb264f4ddf11c529326a0c9eaca9
Robert Kridel <robert@Roberts-MacBook-Pro-2.local>
2021-04-06 13:15:15 -0400

    added median

--- Code/Analysis/Methylation/RK-plot-purity.R
+++ Code/Analysis/Methylation/RK-plot-purity.R
@@ -16,7 +16,7 @@ purity <- read.csv("methylation/Purity_281probes_10Jan2020.csv") %>%
 
 purity %>%
   group_by(SNFClust) %>%
-  summarize(mean = mean(purity))
+  summarize(mean = mean(purity), median = median(purity))
 
 p <- purity %>%
   select(Sample_ID, 'purity (%)' = purity, 'SNF cluster' = SNFClust) %>%
@@ -24,5 +24,5 @@ p <- purity %>%
             add = "jitter", palette = c("1" = "#4363d8", "2" = "#f58231")) +
   ggtitle("Purity estimated from methylation") +
   theme(legend.position = "none")
-p + stat_compare_means(method = "wilcox.test")
+p + stat_compare_means(method = "t.test")
 ggsave("img/purity_SNFClust.pdf", width = 3.5, height = 3.5)
