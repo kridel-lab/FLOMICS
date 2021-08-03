f7680502e93a713c20af186a5f9ddd8ebc735c5c
Anjali Silva <anjali.silva@uhnresearch.ca>
2021-08-03 13:58:45 -0400

    Update 4_SummarizationMvalues.R

--- Code/Analysis/Methylation/4_SummarizationMvalues.R
+++ Code/Analysis/Methylation/4_SummarizationMvalues.R
@@ -1,3 +1,4 @@
+# Updated 3 Aug 2021
 # Updated 12 Feb 2019
 # Function: Summarize M values (mainly used for statistical analysis) by genes rather than probes. 
 # Author: Anjali Silva
@@ -10,8 +11,8 @@
 # Output: 
 # SummarizedMvalueMatrix: A matrix of size genes x patients, with summarized probe data for M values.
 
-SummarizationMvalues <- function(MvalueMatrix, 
-                                 ClinicalFile) {
+SummarizationMvalues4 <- function(MvalueMatrix, 
+                                  ClinicalFile) {
   
   # Loading needed packages
   library(minfi)
@@ -22,57 +23,53 @@ SummarizationMvalues <- function(MvalueMatrix,
   ann <- minfi::getAnnotation(IlluminaHumanMethylationEPICanno.ilm10b2.hg19)
   
   # Matching rownames in methylation file with corresponding location in annotation file
-  match_ids_methylation <- match(rownames(MvalueMatrix), ann$Name)
+  matchIdsMethylation <- match(rownames(MvalueMatrix), ann$Name)
   
   # Using matched rownames, get the gene names from annotation file for corresponding probe names in methylation file
   # colnames(ann) # UCSC_RefGene_Name define genes, which is column 22
-  #length(ann[match_ids_methylation,22]) # 569817
+  #length(ann[matchIdsMethylation,22]) # 569817
   
   # some annotations are missing. This was confirmed using excel sheet too. This is due to the way annotation file is.
-  missing_annotation_methylation <- which(ann[match_ids_methylation, 22] == "")
-  # length(missing_annotation_methylation) # 160094
+  missingAnnotationMethylation <- which(ann[matchIdsMethylation, 22] == "")
+  # length(missingAnnotationMethylation) # 160094
   
   # getting annotations with missing gene rows removed
-  ann_missing_removed <- ann[match_ids_methylation[- missing_annotation_methylation], ]
-  # dim(ann_missing_removed)[1] # 46; this makes sense as 569817 - 160094 = 409723
+  annMissingRemoved <- ann[matchIdsMethylation[- missingAnnotationMethylation], ]
+  # dim(annMissingRemoved)[1] # 46; this makes sense as 569817 - 160094 = 409723
   # gettting probes, such that probes with missing annotation removed
-  MvalueMatrix_missing_removed <- MvalueMatrix[- missing_annotation_methylation, ]
-  # dim(MvalueMatrix_missing_removed) #  409723     173
-  # match(rownames(MvalueMatrix_missing_removed), ann_missing_removed[,4]) # in order
+  MvalueMatrixMissingRemoved <- MvalueMatrix[- missingAnnotationMethylation, ]
+  # dim(MvalueMatrixMissingRemoved) #  409723     173
+  # match(rownames(MvalueMatrixMissingRemoved), annMissingRemoved[,4]) # in order
   
   # only taking first feature
-  ann_missing_removed_truncated <- sub ("\\;.*", "" , ann_missing_removed[ , 22] ) 
-  # length(ann_missing_removed_truncated) # 409723
+  annMissingRemovedTruncated <- sub ("\\;.*", "" , annMissingRemoved[ , 22] ) 
+  # length(annMissingRemovedTruncated) # 409723
   
   # CollapseRows allows to select one representative row per group
-  Mvalues_missingremoved_collapsed_truncated <- WGCNA::collapseRows(MvalueMatrix_missing_removed,
-                                                             rowGroup = ann_missing_removed_truncated,
-                                                             rowID = rownames(MvalueMatrix_missing_removed),
+  MvaluesMissingremovedCollapsedTruncated <- WGCNA::collapseRows(MvalueMatrixMissingRemoved,
+                                                             rowGroup = annMissingRemovedTruncated,
+                                                             rowID = rownames(MvalueMatrixMissingRemoved),
                                                              method = "Average")
-  Mvalues_missingremoved_collapsed2_truncated <- data.frame(Mvalues_missingremoved_collapsed_truncated$group2row, 
-                                                            Mvalues_missingremoved_collapsed_truncated$datETcollapsed)
+  MvaluesMissingremovedCollapsed2Truncated <- data.frame(MvaluesMissingremovedCollapsedTruncated$group2row, 
+                                                            MvaluesMissingremovedCollapsedTruncated$datETcollapsed)
   
-  colnames(Mvalues_missingremoved_collapsed2_truncated)[1] <- "NAME"
+  colnames(MvaluesMissingremovedCollapsed2Truncated)[1] <- "NAME"
   
-  Mvalues_missingremoved_collapsed2_truncated$selectedRowID <- NULL
+  MvaluesMissingremovedCollapsed2Truncated$selectedRowID <- NULL
   
-  Mvalues_missingremoved_collapsed2_truncated <- Mvalues_missingremoved_collapsed2_truncated[ , 
-                                                 order(names(Mvalues_missingremoved_collapsed2_truncated))]
+  MvaluesMissingremovedCollapsed2Truncated <- MvaluesMissingremovedCollapsed2Truncated[ , 
+                                                 order(names(MvaluesMissingremovedCollapsed2Truncated))]
   
-  Mvalues_missingremoved_collapsed2_truncated <- Mvalues_missingremoved_collapsed2_truncated[ , 
-                                                 colnames(Mvalues_missingremoved_collapsed2_truncated) != "NAME"]
+  MvaluesMissingremovedCollapsed2Truncated <- MvaluesMissingremovedCollapsed2Truncated[ , 
+                                                 colnames(MvaluesMissingremovedCollapsed2Truncated) != "NAME"]
   
-  # dim(Mvalues_missingremoved_collapsed2_truncated) #  26589    30
+  # dim(MvaluesMissingremovedCollapsed2Truncated) #  26589    30
   
-  # tail(Mvalues_missingremoved_collapsed2_truncated)
+  # tail(MvaluesMissingremovedCollapsed2Truncated)
   
-  RESULTS <- list(SummarizedMvalueMatrix = Mvalues_missingremoved_collapsed2_truncated)
+  RESULTS <- list(SummarizedMvalueMatrix = MvaluesMissingremovedCollapsed2Truncated)
   
   class(RESULTS) <- "SummarizationMvalues_ASilva"
   return(RESULTS) 
-
-  
 }
-
-
-
+# [END]
