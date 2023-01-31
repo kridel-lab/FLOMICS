63a193707fbc6185559306508fa51f69c2fcee95
victoriashelton <Victoria.Shelton@uhn.ca>
2023-01-31 10:54:42 -0500

    updated paths

--- DNAseq/Calling_Variants_Pipeline/snakemake_CAPSEQ_analysis.sh
+++ DNAseq/Calling_Variants_Pipeline/snakemake_CAPSEQ_analysis.sh
@@ -5,8 +5,8 @@
 #SBATCH -c 6
 #SBATCH --mem=20000M
 #SBATCH -t 5-00:00 # Runtime in D-HH:MM
-#SBATCH -J ppE2408-CAPSEQ
-#SBATCH -o /cluster/projects/kridelgroup/FLOMICS/DATA/TargetedDNAseq/CAPSEQ_Pipeline_OUTPUT/E2408-Tumor-Only/run001/slurm-outs/%x-%j.out #redirect job output (both stdout and stderr) to a file called “<job name>-<job id>.out”
+#SBATCH -J Calling-Variants-CAPSEQ
+#SBATCH -o /CAPSEQ_Pipeline_OUTPUT/slurm-outs/%x-%j.out #redirect job output (both stdout and stderr) to a file called “<job name>-<job id>.out”
 
 #load required modules
 module load picard/2.10.9
@@ -15,12 +15,12 @@ module load python3/3.7.2
 module load samtools
 module load snakemake/5.20.1
 
-cd /cluster/home/vshelton/CapSeq_pipeline/TargetedDNAseq_pipeline
+cd /Calling_Variants_Pipeline
 
-snakemake -s /cluster/home/vshelton/CapSeq_pipeline/TargetedDNAseq_pipeline/CAPSEQ_Pipeline_BC.smk \
+snakemake -s /Calling_Variants_Pipeline/CAPSEQ_Pipeline.smk \
     -j157 \
     --latency-wait 432000 \
-    --cluster-config /cluster/home/vshelton/CapSeq_pipeline/TargetedDNAseq_pipeline/cluster.yaml \
+    --cluster-config /Calling_Variants_Pipeline/cluster.yaml \
     --cluster-sync "sbatch -p {cluster.partition} --mem={cluster.mem} -t {cluster.time} -c {cluster.ncpus} -o {cluster.out}" \
     --rerun-incomplete \
     --nolock
