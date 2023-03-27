aea5939e58121c9263fb888598492f2045344e5b
victoriashelton <Victoria.Shelton@uhn.ca>
2023-03-27 14:24:51 -0400

    cleaned up

--- Figures/Fig5.R
+++ Figures/Fig5.R
@@ -8,91 +8,106 @@ lapply(packages, require, character.only = TRUE)
 
 date <- Sys.Date()
 
-setwd("~/github/FLOMICS/")
+setwd("~/your working directory/FLOMICS/")
 
 # Read in RNAseq QC
-rnaseq.samples.passing.qc <-
-  read.table("RNAseq/2022_combined_RNAseq_total365/passed_290/metadata_passed_290.txt", "\t", header = TRUE) %>% .$id #290
+rnaseq_samples_passing_qc <-
+  read.table("metadata_passed_290.txt", "\t", header = TRUE) %>% .$id
 
 # Read in clusters
-clusters <- read.csv("DNAseq/CAPSEQ/SNV_Cluster_Analysis/2022-12-20_UHN_E4402_PLOSMED-T-O_E2408-T-O_codingBCL2only_713-samples/2023-01-19_most_confident_cluster_run_5_clusters/2023-01-19_14_GMM_Cluster_Labels_flexmix_clusters.csv") %>%
+clusters <- read.csv("GMM_Cluster_Labels_flexmix_clusters.csv") %>%
   select(SAMPLE_ID, ClusterAIC, cohort)
 
 # Read in sample annotation
-sample.annotation <- read.table("metadata/20221228_sample_annotations.txt", header = T, sep = "\t")
-FL.cases <- sample.annotation %>% filter(TYPE == "FL") %>% .$SAMPLE_ID
+sample_annotation <- read.table("metadata/20221228_sample_annotations.txt",
+ header = TRUE, sep = "\t")
+fl_cases <- sample_annotation %>% filter(TYPE == "FL") %>% .$SAMPLE_ID
 
 # Read in clinical data
 clin <- read.csv("metadata/20220525_clinical_data.csv") %>%
-  mutate(LY_FL_ID = paste0(LY_FL_ID, "_T1")) %>% select(LY_FL_ID, ANN_ARBOR_STAGE)
+  mutate(LY_FL_ID = paste0(LY_FL_ID, "_T1")) %>%
+  select(LY_FL_ID, ANN_ARBOR_STAGE)
 
 # Read in TRUST4 output
-airr.TGL.OICR <- read_tsv("TRUST4/TGL_OICR/IMGT_airr_merged.tsv") %>%
-  mutate(sample_id = ifelse(sample_id %in% c("LY_FL_234", "LY_FL_235", "LY_FL_236", "LY_FL_237", "LY_FL_238",
-                                             "LY_FL_246", "LY_FL_247", "LY_FL_249", "LY_FL_252", "LY_FL_253",
-                                             "LY_FL_254", "LY_FL_255", "LY_FL_256", "LY_FL_257", "LY_FL_258",
-                                             "LY_FL_261"), paste0(sample_id, "_T1"), sample_id))
+airr_tgl_ocir <- read_tsv("TRUST4/TGL_OICR/IMGT_airr_merged.tsv") %>%
+  mutate(sample_id = ifelse(sample_id %in%
+   c("LY_FL_234", "LY_FL_235",
+   "LY_FL_236", "LY_FL_237", "LY_FL_238", "LY_FL_246", "LY_FL_247",
+    "LY_FL_249", "LY_FL_252", "LY_FL_253", "LY_FL_254", "LY_FL_255",
+     "LY_FL_256", "LY_FL_257", "LY_FL_258", "LY_FL_261"),
+      paste0(sample_id, "_T1"), sample_id))
 
-airr.E4402 <- read_tsv("TRUST4/E4402/IMGT_airr_merged.tsv") 
+airr_e4402 <- read_tsv("TRUST4/E4402/IMGT_airr_merged.tsv")
 
 # Combine and retain only those cases for which RNAseq data passed QC
-airr <- rbind(airr.TGL.OICR, airr.E4402) %>% filter(sample_id %in% rnaseq.samples.passing.qc)
+airr <- rbind(airr_tgl_ocir, airr_e4402) %>%
+ filter(sample_id %in% rnaseq_samples_passing_qc)
 
 # Number of cases
-length(unique(airr$sample_id)) # n = 290
+length(unique(airr$sample_id))
 
 #--
 # Identify most common isotype vs clusters
 #--
 
-clonotypes.IGH.FL <- airr %>%
-  filter(sample_id %in% FL.cases) %>%
+clonotypes_igh_fl <- airr %>%
+  filter(sample_id %in% fl_cases) %>%
   filter(grepl("IGH", v_call))
 
-clonotypes.IGH.FL_tot <- clonotypes.IGH.FL %>%
-  group_by(sample_id) %>% summarize(count.tot = sum(consensus_count))
+clonotypes_igh_fl_tot <- clonotypes_igh_fl %>%
+  group_by(sample_id) %>%
+  summarize(count.tot = sum(consensus_count))
 
-summary(clonotypes.IGH.FL_tot$count.tot)
-  
-clonotypes.IGH.FL_VDJgrouped <- clonotypes.IGH.FL %>%
+summary(clonotypes_igh_fl_tot$count.tot)
+
+clonotypes_igh_fl_vdjgrouped <- clonotypes_igh_fl %>%
   mutate(v_call = sub("\\*.*", "", v_call)) %>%
   mutate(d_call = substr(d_call, 1, 5)) %>%
   mutate(j_call = substr(j_call, 1, 5)) %>%
   mutate(c_call = sub("\\*.*", "", c_call)) %>%
   group_by(sample_id, v_call, d_call, j_call, c_call) %>%
   summarize(count = sum(consensus_count))
-dim(clonotypes.IGH.FL_VDJgrouped) # 52472
+dim(clonotypes_igh_fl_vdjgrouped)
 
-clonotypes.IGH.FL_VDJgrouped_dominant.all <- clonotypes.IGH.FL_VDJgrouped %>% 
-  group_by(sample_id) %>% slice_max(n = 1, order_by = count)
-summary(clonotypes.IGH.FL_VDJgrouped_dominant.all$count)
+clonotypes_igh_fl_vdjgrouped_dominant_all <- clonotypes_igh_fl_vdjgrouped %>%
+  group_by(sample_id) %>%
+  slice_max(n = 1, order_by = count)
+summary(clonotypes_igh_fl_vdjgrouped_dominant_alll$count)
 
-clonotypes.IGH.FL_VDJgrouped_dominant <- clonotypes.IGH.FL_VDJgrouped %>% 
-  group_by(sample_id) %>% slice_max(n = 1, order_by = count) %>%
+clonotypes_igh_fl_vdjgrouped_dominant <- clonotypes_igh_fl_vdjgrouped %>%
+  group_by(sample_id) %>%
+  slice_max(n = 1, order_by = count) %>%
   filter(!is.na(c_call)) %>%
   left_join(clonotypes.IGH.FL_tot) %>%
-  mutate(rel.prop.dom = count/count.tot)
-dim(clonotypes.IGH.FL_VDJgrouped_dominant) # 272
+  mutate(rel.prop.dom = count / count.tot)
+dim(clonotypes_igh_fl_vdjgrouped_dominant)
 
 # Determine whether either IgG or IgM expression is enriched in genetic clusters
-df <- clonotypes.IGH.FL_VDJgrouped_dominant %>%
+df <- clonotypes_igh_fl_vdjgrouped_dominant %>%
   mutate(isotype = c_call) %>%
-  left_join(clusters[,c("SAMPLE_ID", "ClusterAIC")], by = c("sample_id" = "SAMPLE_ID")) %>%
+  left_join(clusters[, c("SAMPLE_ID", "ClusterAIC")],
+   by = c("sample_id" = "SAMPLE_ID")) %>%
   filter(!is.na(ClusterAIC)) %>%
   mutate(isotype = ifelse(isotype == "IGHM", "IGHM", "non-IGHM")) %>%
   group_by(ClusterAIC, isotype) %>%
   dplyr::summarize(n = n()) %>%
-  tidyr::spread(ClusterAIC, n) %>% replace(is.na(.), 0)
+  tidyr::spread(ClusterAIC, n) %>%
+  replace(is.na(.), 0)
 
-b1 <- binom.test(as.numeric(df[2,"C1"]), colSums(df[,"C1"]), p = 0.5, alternative = c("two.sided"), conf.level = 0.95)$p.value
+b1 <- binom.test(as.numeric(df[2, "C1"]), colSums(df[, "C1"]), p = 0.5,
+ alternative = c("two.sided"), conf.level = 0.95)$p.value
 b1 <- round(b1, 3)
-b2 <- binom.test(as.numeric(df[2,"C2"]), colSums(df[,"C2"]), p = 0.5, alternative = c("two.sided"), conf.level = 0.95)$p.value
+b2 <- binom.test(as.numeric(df[2, "C2"]), colSums(df[, "C2"]), p = 0.5,
+ alternative = c("two.sided"), conf.level = 0.95)$p.value
 b2 <- round(b2, 3)
-b3 <- binom.test(as.numeric(df[2,"C3"]), colSums(df[,"C3"]), p = 0.5, alternative = c("two.sided"), conf.level = 0.95)$p.value
+b3 <- binom.test(as.numeric(df[2, "C3"]), colSums(df[, "C3"]), p = 0.5,
+ alternative = c("two.sided"), conf.level = 0.95)$p.value
 b3 <- round(b3, 3)
-b4 <- binom.test(as.numeric(df[2,"C4"]), colSums(df[,"C4"]), p = 0.5, alternative = c("two.sided"), conf.level = 0.95)$p.value
+b4 <- binom.test(as.numeric(df[2, "C4"]), colSums(df[, "C4"]), p = 0.5,
+ alternative = c("two.sided"), conf.level = 0.95)$p.value
 b4 <- round(b4, 3)
-b5 <- binom.test(as.numeric(df[2,"C5"]), colSums(df[,"C5"]), p = 0.5, alternative = c("two.sided"), conf.level = 0.95)$p.value
+b5 <- binom.test(as.numeric(df[2, "C5"]), colSums(df[, "C5"]), p = 0.5,
+ alternative = c("two.sided"), conf.level = 0.95)$p.value
 b5 <- formatC(b5, format = "e", digits = 2)
 
 #--
@@ -105,23 +120,26 @@ ann_text <- data.frame(ClusterAIC = c("C1", "C2", "C3", "C4", "C5"),
                        label = paste0("P = ", c(b1, b2, b3, b4, b5)),
                        n = rep(c(42), 5))
 
-p1 <- clonotypes.IGH.FL_VDJgrouped_dominant %>%
+p1 <- clonotypes_igh_fl_vdjgrouped_dominant %>%
   mutate(isotype = c_call) %>%
-  left_join(clusters[,c("SAMPLE_ID", "ClusterAIC")], by = c("sample_id" = "SAMPLE_ID")) %>%
+  left_join(clusters[, c("SAMPLE_ID", "ClusterAIC")],
+   by = c("sample_id" = "SAMPLE_ID")) %>%
   filter(!is.na(ClusterAIC)) %>%
-  mutate(isotype = ifelse(isotype %in% c("IGHG1", "IGHG2", "IGHG3", "IGHG4"), "IGHG",
+  mutate(isotype = ifelse(isotype %in%
+                     c("IGHG1", "IGHG2", "IGHG3", "IGHG4"), "IGHG",
                    ifelse(isotype %in% c("IGHA1", "IGHA2"), "IGHA",
                    ifelse(isotype == "IGHM", "IGHM", isotype)))) %>%
   group_by(ClusterAIC, isotype) %>%
   dplyr::summarize(n = n()) %>%
   mutate(isotype = factor(isotype, levels = c("IGHM", "IGHG", "IGHA"))) %>%
-  ggplot(aes (x = isotype, y = n, fill = isotype, label = n))+
+  ggplot(aes(x = isotype, y = n, fill = isotype, label = n)) +
   geom_bar(stat = "identity") +
   geom_text(size = 3, position = position_stack(vjust = 0.5)) +
   facet_wrap(~ClusterAIC, ncol = 5) +
   geom_text(data = ann_text, size = 2, aes(label = label, x = isotype, y = n)) +
   theme_bw() +
-  theme(text = element_text(size = 10)) + ylab("Number of samples with given isotype") +
+  theme(text = element_text(size = 10)) +
+  ylab("Number of samples with given isotype") +
   theme(axis.text.y = element_text(size = 5),
         axis.title.y = element_text(size = 7),
         axis.text.x = element_text(size = 5),
@@ -136,39 +154,50 @@ p1
 # Plot the V gene identity in IgM and IgG-expressing cases
 #--
 
-df2 <- clonotypes.IGH.FL %>%
+df2 <- clonotypes_igh_fl %>%
   mutate(v_call = sub("\\*.*", "", v_call)) %>%
   mutate(d_call = substr(d_call, 1, 5)) %>%
   mutate(j_call = substr(j_call, 1, 5)) %>%
   mutate(c_call = sub("\\*.*", "", c_call)) %>%
   group_by(sample_id, v_call, d_call, j_call, c_call, v_identity) %>%
   summarize(count = sum(consensus_count)) %>%
-  group_by(sample_id) %>% slice_max(n = 1, order_by = count) %>%
+  group_by(sample_id) %>%
+  slice_max(n = 1, order_by = count) %>%
   filter(!is.na(c_call)) %>%
   left_join(clonotypes.IGH.FL_tot) %>%
-  mutate(rel.prop.dom = count/count.tot) %>%
-  left_join(clusters[,c("SAMPLE_ID", "ClusterAIC")], by = c("sample_id" = "SAMPLE_ID")) %>%
-  filter(ClusterAIC == factor(ClusterAIC, levels = c("C1", "C2", "C3", "C4", "C5"))) %>%
+  mutate(rel.prop.dom = count / count.tot) %>%
+  left_join(clusters[, c("SAMPLE_ID", "ClusterAIC")],
+   by = c("sample_id" = "SAMPLE_ID")) %>%
+  filter(ClusterAIC == factor(ClusterAIC,
+   levels = c("C1", "C2", "C3", "C4", "C5"))) %>%
   filter(!is.na(ClusterAIC)) %>%
   data.frame()
 
-df2 %>% mutate(c_call = ifelse(c_call %in% c("IGHG1", "IGHG2", "IGHG3", "IGHG4"), "IGHG",
-                        ifelse(c_call %in% c("IGHA1", "IGHA2"), "IGHA", c_call))) %>%
+df2 %>% mutate(c_call = ifelse(c_call %in%
+                         c("IGHG1", "IGHG2", "IGHG3", "IGHG4"), "IGHG",
+                        ifelse(c_call %in%
+                         c("IGHA1", "IGHA2"), "IGHA", c_call))) %>%
   filter(c_call %in% c("IGHM", "IGHG")) %>%
-  group_by(c_call) %>% summarize(mean(v_identity))
+  group_by(c_call) %>%
+   summarize(mean(v_identity))
 
-p2 <- df2 %>% mutate(c_call = ifelse(c_call %in% c("IGHG1", "IGHG2", "IGHG3", "IGHG4"), "IGHG",
-                              ifelse(c_call %in% c("IGHA1", "IGHA2"), "IGHA", c_call))) %>%
+p2 <- df2 %>% mutate(c_call = ifelse(c_call %in%
+                               c("IGHG1", "IGHG2", "IGHG3", "IGHG4"), "IGHG",
+                              ifelse(c_call %in%
+                               c("IGHA1", "IGHA2"), "IGHA", c_call))) %>%
   filter(c_call %in% c("IGHM", "IGHG")) %>%
-  ggboxplot("c_call", "v_identity", color = "c_call", palette = c("#a6dba0", "#c2a5cf"), ncol = 5, lwd = 0.5, add = "jitter", add.params = list(size = 0.1, jitter = 0.2)) +
-  stat_compare_means(method = "wilcox.test", size = 2, label.x.npc = 0.25, label.y.npc = 0.95) +
+  ggboxplot("c_call", "v_identity", color = "c_call",
+   palette = c("#a6dba0", "#c2a5cf"), ncol = 5, lwd = 0.5, add = "jitter",
+    add.params = list(size = 0.1, jitter = 0.2)) +
+  stat_compare_means(method = "wilcox.test", size = 2, label.x.npc = 0.25,
+   label.y.npc = 0.95) +
   theme_bw() +
   theme(axis.title.x = element_text(size = 7),
         axis.text.x = element_text(size = 7),
         axis.title.y = element_text(size = 7),
         axis.text.y = element_text(size = 7),
-        legend.position = "none") + 
-  ylab("V gene identity (%)") + xlab("Isotype") + ylim(70,110)
+        legend.position = "none") +
+  ylab("V gene identity (%)") + xlab("Isotype") + ylim(70, 110)
 
 p2
 
@@ -176,11 +205,14 @@ p2
 # Plot the V gene identity
 #--
 
-my_comparisons <- list( c("C3", "C1"), c("C3", "C2"), c("C3", "C4"), c("C3", "C5"))
+my_comparisons <- list(
+  c("C3", "C1"), c("C3", "C2"), c("C3", "C4"), c("C3", "C5"))
 
-p3 <- df2 %>% mutate(ClusterAIC = factor(ClusterAIC, levels = c("C1", "C2", "C3", "C4", "C5"))) %>%
+p3 <- df2 %>% mutate(ClusterAIC = factor(ClusterAIC,
+ levels = c("C1", "C2", "C3", "C4", "C5"))) %>%
   select(Cluster = ClusterAIC, v_identity) %>%
-  ggboxplot("Cluster", "v_identity", color = "Cluster", ncol = 5, lwd = 0.3, add = "jitter", add.params = list(size = 0.1, jitter = 0.2)) +
+  ggboxplot("Cluster", "v_identity", color = "Cluster", ncol = 5, lwd = 0.3,
+   add = "jitter", add.params = list(size = 0.1, jitter = 0.2)) +
   stat_compare_means(comparisons = my_comparisons,
                      method = "wilcox.test",
                      size = 2, label.y = c(100, 103, 106, 109)) +
@@ -188,10 +220,14 @@ p3 <- df2 %>% mutate(ClusterAIC = factor(ClusterAIC, levels = c("C1", "C2", "C3"
                      size = 2,
                      label.x.npc = 0.01, label.y.npc = 0.01) +
   theme_bw() +
-  theme(axis.title.x = element_text(size = 7), axis.text.x = element_text(size = 7),
-        axis.title.y = element_text(size = 7), axis.text.y = element_text(size = 7),
+  theme(axis.title.x = element_text(size = 7),
+   axis.text.x = element_text(size = 7),
+        axis.title.y = element_text(size = 7),
+         axis.text.y = element_text(size = 7),
         legend.position = "none") +
   ylab("V gene identity (%)")
 
-g <- gridExtra::grid.arrange(p1, gridExtra::arrangeGrob(p2, p3, ncol = 2, widths = c(0.8, 1)), ncol = 1)
-ggsave(file = paste0("img/",  date, " Fig5.pdf"), g, width = 14, height = 12, units = "cm")
+g <- gridExtra::grid.arrange(p1, gridExtra::arrangeGrob(p2, p3,
+ ncol = 2, widths = c(0.8, 1)), ncol = 1)
+ggsave(file = paste0("img/",  date, " Fig5.pdf"), g, width = 14,
+ height = 12, units = "cm")
