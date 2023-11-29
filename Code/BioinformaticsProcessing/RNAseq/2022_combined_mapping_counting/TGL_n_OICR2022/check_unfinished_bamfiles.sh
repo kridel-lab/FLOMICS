da27ad115e8ca6696c44fa269238697c624e2e3e
victoriashelton <Victoria.Shelton@uhn.ca>
2023-11-29 13:20:18 -0500

    deleting directory

--- Code/BioinformaticsProcessing/RNAseq/2022_combined_mapping_counting/TGL_n_OICR2022/check_unfinished_bamfiles.sh
+++
@@ -1,9 +0,0 @@
-file="done_ids.txt";
-
-for i in `cat ${file}`
-do
-  echo $i
-  `grep $i bam_files_list.txt >>done_bam.txt`
-done
-
-`grep -vFf done_bam.txt bam_files_list.txt >unfinished_bam_list.txt`
--- Code/Tools/RESET/README.md
+++
@@ -1,1 +0,0 @@
-This is the official repository of the R package xxxxxxx - xxxxxx - a methodology to xxxxxxx
